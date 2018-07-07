3269da5766a9cadbe3e77fc15db9b3ae66107382
allioli <allioli@allioli.com>
2018-07-07 13:29:07 +0200

    Initial commit for API testing task

---
+++ api-tests/features/top_rated_movies.feature
@@ -0,0 +1,27 @@
+
+Feature: MovieDB API Top Rated Movies
+
+  Scenario: Default query
+    Given I create moviedb API request for "top_rated"
+    When I perform request to movie-db API
+    Then response code is "200"
+    And response contains "page" equal to number "1"
+    And response contains movie with "id" equal to number "19404"
+    And movies are sorted by "vote_average" in descending order
+
+  Scenario: Invalid API key
+    Given I create moviedb API request for "top_rated"
+    And I add parameter "custom_api_key" with value "555555"
+    When I perform request to movie-db API
+    Then response code is "401"
+    And response contains "status_code" equal to number "7"
+    And response contains "status_message" equal to "Invalid API key: You must be granted a valid key."
+
+  Scenario: Invalid API resource
+    Given I create moviedb API request for "top_scraped"
+    When I perform request to movie-db API
+    Then response code is "404"
+    And response contains "status_code" equal to number "34"
+    And response contains "status_message" equal to "The resource you requested could not be found."
+
+                                   
---
+++ api-tests/steps/top_rated_movies_steps.py
@@ -0,0 +1,98 @@
+
+import requests
+from behave import given, when, then
+from hamcrest import equal_to, assert_that, starts_with, less_than_or_equal_to
+
+
+api_key = '419af08f20d7174c0a764c55e22c403a'
+base_movie_db_api_base_url = 'https://api.themoviedb.org/3/movie/'
+
+
+def compose_mobiedb_request_url(context):
+
+    url = base_movie_db_api_base_url + context.request_params['api_resource']
+
+    if('custom_api_key' in context.request_params):
+        url += ('?api_key=' + context.request_params['custom_api_key'])
+    else:
+        url += '?api_key=' + api_key
+
+    if('language' in context.request_params):
+        url += ('&language=' + context.request_params['language'])
+
+    if('region' in context.request_params):
+        url += ('&region=' + context.request_params['region'])
+    
+    if('page' in context.request_params):
+        url += ('&page=' + context.request_params['page'])
+
+    return url
+
+
+@given(u'I create moviedb API request for "{api_resource}"')
+def step_given_I_create_moviedb_api_request(context, api_resource):
+
+    context.request_params = {'api_resource': api_resource}
+
+
+@given(u'I add parameter "{parameter_name}" with value "{parameter_value}"')
+def step_given_I_add_parameter_with_value(context, parameter_name, parameter_value):
+
+    context.request_params[parameter_name] = parameter_value
+
+
+@when(u'I perform request to movie-db API')
+def step_when_I_perform_request_to_movie_db_api(context):
+
+    request_url = compose_mobiedb_request_url(context)
+    context.response = requests.get(request_url)
+    context.response_json = context.response.json()
+
+
+@then(u'response code is "{response_code:d}"')
+def step_then_response_code(context, response_code):
+
+    assert_that(context.response.status_code, equal_to(response_code))
+
+
+@then(u'response contains "{field}" equal to number "{value:d}"')
+def step_then_response_contains_field_number(context, field, value):
+
+    assert_that(context.response_json[field], equal_to(value))
+
+@then(u'response contains "{field}" equal to "{value}"')
+def step_then_response_contains_field(context, field, value):
+
+    assert_that(context.response_json[field], equal_to(value))
+
+
+@then(u'response contains movie with "{field}" equal to number "{value:d}"')
+def step_then_response_contains_movie_with_field_equal_number(context, field, value):
+
+    assert_that(context.response_json['results'][0][field], equal_to(value))
+
+
+@then(u'response contains movie with "{field}" equal to "{value}"')
+def step_then_response_contains_movie_with_field_equal(context, field, value):
+
+    assert_that(context.response_json['results'][0][field], equal_to(value))
+
+
+@then(u'response contains movie with "{field}" starting with "{value}"')
+def step_then_response_contains_movie_with_field_starting(context, field, value):
+
+    assert_that(context.response_json['results'][0][field], starts_with(value))
+
+@then(u'movies are sorted by "{field}" in descending order')
+def step_then_response_contains_movie_with_field_starting(context, field):
+
+    num_results = len(context.response_json['results'])
+    if (num_results > 1):
+        for index in range(1, num_results):
+            # Check that movie value for field is lower or equal than on the previous one
+            movie_value = context.response_json['results'][index][field]
+            previous_movie_value =  context.response_json['results'][index-1][field]
+            assert_that(movie_value, less_than_or_equal_to(previous_movie_value), 'Movies not correctly sorted')
+
+     
+
